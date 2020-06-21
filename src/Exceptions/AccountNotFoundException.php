8c9eeece496da8a215725970992f172159fce0b9
ChurakovMike <churakovmike@mail.ru>
2020-06-21 16:06:58 +0700

    + add transfer money function

---
+++ src/Exceptions/AccountNotFoundException.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace ChurakovMike\Finance\Exceptions;
+
+use Exception;
+
+/**
+ * Class AccountNotFound
+ * @package ChurakovMike\Finance\Exceptions
+ */
+class AccountNotFoundException extends Exception
+{
+}
--- src/Traits/AccountBillable.php
+++ src/Traits/AccountBillable.php
@@ -5,6 +5,8 @@ declare(strict_types=1);
 namespace ChurakovMike\Finance\Traits;
 
 use App\User;
+use ChurakovMike\Finance\Models\Account;
+use Illuminate\Support\Facades\DB;
 
 /**
  * This trait must be used to the user model.
@@ -13,18 +15,70 @@ use App\User;
  */
 trait AccountBillable
 {
+    /**
+     * @param User $user
+     * @param $amount
+     * @param null $accountType
+     * @throws \Throwable
+     */
     public function transferTo(User $user, $amount, $accountType = null)
     {
-        //
+        if (is_null($accountType)) {
+            $accountType = Account::TYPE_MAIN;
+        }
+
+        DB::beginTransaction();
+        try {
+            $account = $this->getAccounts()
+                ->where('type_id', $accountType)
+                ->firstOrFail();
+
+            $transferAccount = $user->getAccounnts()
+                ->where('type_id', $accountType)
+                ->firstOrFail();
+
+            $account->update([
+                'balance' => $account->balance - $amount,
+            ]);
+
+            $transferAccount->update([
+                'balance' => $account->balance + $amount,
+            ]);
+            DB::commit();
+        } catch (\Throwable $exception) {
+            DB::rollBack();
+            throw $exception;
+        }
     }
 
+    /**
+     * @param $amount
+     * @throws \Throwable
+     */
     public function transferToTransit($amount)
     {
-        //
+        DB::beginTransaction();
+        try {
+            $transitAccount = $this->getAccounts()
+                ->where('type_id', Account::TYPE_TRANSIT)
+                ->firstOrFail();
+
+            $transitAccount->update([
+                'balance' => $transitAccount->balance + $amount,
+            ]);
+
+            DB::commit();
+        } catch (\Throwable $exception) {
+            DB::rollBack();
+            throw $exception;
+        }
     }
 
+    /**
+     * @return mixed
+     */
     public function getAccounts()
     {
-        //
+        return $this->hasMany(Account::class);
     }
 }
